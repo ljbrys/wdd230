5e59380b6d608c2f173f79eae469202dafcb2917
ljbrys <bry21010@byui.edu>
2022-04-24 10:56:00 -0400

    10-55

--- js/lastModified.js
+++ js/lastModified.js
@@ -1,1 +1,5 @@
 let oLastModif = new Date(document.lastModified);
+//document.write("this is only a test to make sure javascipt is working - it will be commented out upon success")
+
+
+document.getElementById('dateLastModified').innerHTML = oLastModif;
