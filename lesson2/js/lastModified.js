9b49423a9b1a54d0ecdbb8de3b9e1e5bbba19cff
ljbrys <104159889+ljbrys@users.noreply.github.com>
2022-05-02 01:39:57 -0400

    update

---
+++ lesson2/js/lastModified.js
@@ -0,0 +1,6 @@
+let oLastModif = new Date(document.lastModified);
+//document.write("this is only a test to make sure javascipt is working - it will be commented out upon success")
+
+
+document.getElementById('dateLastModified').innerHTML = "Last date modified = " + oLastModif;
+//document.write("Last date modified is"+ oLastModif)
